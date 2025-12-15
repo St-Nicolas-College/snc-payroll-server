7212b841fe8edcc90613e5b609083978e3ac0c70
almar26 <gomezalmar26@gmail.com>
2025-12-15 18:08:32 +0800

    added update cash advance balance to payslip lifecycle afterCreate

--- src/api/payslip/content-types/payslip/lifecycles.js
+++ src/api/payslip/content-types/payslip/lifecycles.js
@@ -4,24 +4,6 @@ module.exports = {
     async beforeCreate(event) {
         const { data } = event.params;
         console.log(data.employee.set[0].id)
-        // const existing = await strapi.db.query("api::payslip.payslip").findOne({
-        //     where: {
-        //         employee: data.employee.set[0].id,
-        //         payroll_period: data.payroll_period.set[0].id
-        //     }
-        // });
-
-        // const myPayload = {
-        //     message: " payslip already exist for this employee and payroll period.",
-        //     status: 500
-        // }
-
-        // if (existing) {
-        //     //return event.body = myPayload
-        //     throw new ApplicationError("A payslip already exist for this employee and payroll period.");
-        // }
-        
-
         const exists = await strapi.entityService.findMany("api::payslip.payslip", {
             filters: {
                 employee: data.employee.set[0].id,
@@ -35,4 +17,22 @@ module.exports = {
             throw new ApplicationError("A payslip already exist for this employee and payroll period.");
         }
     },
+
+    async afterCreate(event) {
+        const { result, params } = event
+
+        // const employeeId = result.employee?.id || result.employee
+        const employeeId = params.data.employee
+        //const newBalance = result
+        // console.log('Employee ID: ', employeeId)
+        // console.log('Payslip: ', newBalance)
+
+        //const newCashAdvanceBalance = Number(result.cash_advance_balance) - Number(result.cash_advance_deduction);
+        await strapi.db.query("api::employee.employee").update({
+            where: { id: employeeId.set[0].id },
+            data: { cash_advance_balance: result.cash_advance_balance}
+        })
+
+        console.log(`Updated cash advance balance for employee ${employeeId.set[0].id}: ${result.cash_advance_balance}`);
+    }
 }
