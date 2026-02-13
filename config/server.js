e312d38146b0654c7b2a09a89044c08fe90adc43
almar26 <gomezalmar26@gmail.com>
2026-02-13 16:29:41 +0800

    added url in server config

--- config/server.js
+++ config/server.js
@@ -2,12 +2,12 @@ module.exports = ({ env }) => ({
   host: env('STRAPI_HOST', '0.0.0.0'),
   port: env.int('STRAPI_PORT', 1337),
   proxy: true,
-  trustProxy: true,
+  //trustProxy: true,
   app: {
     keys: env.array('APP_KEYS'),
   },
-  // webhooks: {
-  //   populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
-  // },
-  // url: env("STRAPI_BACKEND_BASEURL")
+  webhooks: {
+    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
+  },
+  url: env("STRAPI_BACKEND_BASEURL")
 });
