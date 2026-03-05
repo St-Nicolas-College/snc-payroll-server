673b981c4cd83fc29f88d66fdc68d454c53f1491
almar26 <gomezalmar26@gmail.com>
2026-03-05 14:58:13 +0800

    update config middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -6,11 +6,12 @@ module.exports = [
     name: 'strapi::cors',
     config: {
       origin: [
+        'https://snc-payroll-ui.vercel.app',
         'https://payroll.snc.edu.ph',
       ], // Nuxt 4 URL
       credentials: true,
       methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization'],
+      headers: '*',
     },
   },
   'strapi::poweredBy',
